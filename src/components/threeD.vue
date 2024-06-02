d994122764f94ae505c3c13080be09db399c5c03
BhJaipa <105307427+BhJaipal@users.noreply.github.com>
2024-06-02 12:53:57 +0530

    gitf workings

--- src/components/threeD.vue
+++ src/components/threeD.vue
@@ -1,45 +1,7 @@
 <script setup lang="ts">
 // @ts-nocheck
-import { OrbitControls } from "@tresjs/cientos";
-import { useFBX } from "@tresjs/cientos";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { OrbitControls, GLTFModel, vLog } from "@tresjs/cientos";
 import * as THREE from "three";
-
-import { useGLTF, useTweakPane } from "@tresjs/cientos";
-import { useRenderLoop } from "@tresjs/core";
-import { shallowRef, watch } from "vue";
-
-const { scene: planet } = await useGLTF(
-	"https://raw.githubusercontent.com/Tresjs/assets/main/models/gltf/low-poly/planet.gltf"
-);
-
-const { pane } = useTweakPane();
-
-const planetRef = shallowRef();
-
-planet.traverse((child) => {
-	if (child.isMesh) {
-		child.receiveShadow = true;
-	}
-});
-
-watch(
-	() => planetRef.value,
-	(planet) => {
-		if (!planet) return;
-		pane.addInput(planetRef.value, "visible", { label: "Planet" });
-	}
-);
-
-const { onLoop } = useRenderLoop();
-
-onLoop(({ delta }) => {
-	if (!planet) return;
-	planet.rotation.y += delta * 0.04;
-	planet.rotation.z += delta * 0.02;
-	planet.rotation.x += delta * 0.05;
-	planet.updateMatrixWorld();
-});
 </script>
 <script lang="ts">
 export default {
@@ -48,28 +10,43 @@ export default {
 </script>
 <template>
 	<div class="three-d">
-		<!--
-			// let scene = new THREE.Scene();
-// let loader = new FBXLoader();
-// loader.load("/Charizard/CharizardMegaX.FBX", function (object) {
-// 	scene.add(object);
-// });
-
-		-->
 		<TresCanvas clear-color="#88F">
 			<TresPerspectiveCamera :position="[0, 0, 5]" />
-			<OrbitControls :enable-zoom="false" />
-			<primitive :object="model" />
-			<TresMesh ref="planetRef" v-bind="planet" />
-			<TresMesh :rotation="[Math.PI / 3, 0, 0]" :position="[-2, -2, 0]">
-				<TresCircleGeometry :args="[0.7, 32]" />
+			<OrbitControls :enable-zoom="false" :enable-rotate="false" />
+			<TresMesh
+				:rotation="[Math.PI / 3, 0, 0]"
+				:position="[-2, -2, 0]"
+				receive-shadow
+			>
+				<TresPlaneGeometry :args="[2, 2, 2, 2]" />
 				<TresMeshBasicMaterial color="#0f0" />
 			</TresMesh>
-			<TresMesh :rotation="[-Math.PI / 2.2, 0, 0]" :position="[2, 0, 0]">
-				<TresCircleGeometry :args="[0.7, 32]" />
+			<TresMesh :position="[2, 2, 0]" :rotation="[0, Math.PI, 0]">
+				<TresConeGeometry :args="[1, 1.5, 3]" />
+				<TresMeshToonMaterial color="#82DBC5" />
+			</TresMesh>
+			<TresMesh
+				:rotation="[-Math.PI / 2.2, 0, 0]"
+				:position="[2, 0, 0]"
+				receive-shadow
+			>
+				<TresPlaneGeometry :args="[2, 2, 2, 2]" />
 				<TresMeshBasicMaterial color="#0f0" />
 			</TresMesh>
-			<!-- <TresMesh v-bind="scene" /> -->
+			<!-- <Suspense>
+				<GLTFModel
+					path="/planet.gltf"
+					:position="[-2, 0, 0]"
+					v-log
+					cast-shadow
+				/>
+			</Suspense> -->
+			<TresDirectionalLight
+				:position="[0, 1, 0]"
+				:intensity="1"
+				cast-shadow
+			/>
+			<TresAmbientLight :intensity="0.5" />
 		</TresCanvas>
 	</div>
 </template>
