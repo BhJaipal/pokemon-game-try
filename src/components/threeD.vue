cf9565c87cefcd5f5154601e2eefa256b2c9736d
BhJaipa <105307427+BhJaipal@users.noreply.github.com>
2024-06-02 11:15:59 +0530

    working

--- src/components/threeD.vue
+++ src/components/threeD.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
 // @ts-nocheck
-import { OrbitControls, Circle } from "@tresjs/cientos";
-import { vLog } from "@tresjs/core";
-import { shallowRef } from "vue";
-const boxRef = shallowRef();
+import { OrbitControls } from "@tresjs/cientos";
+import { useFBX } from "@tresjs/cientos";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import * as THREE from "three";
+
+import { useGLTF, useTweakPane } from "@tresjs/cientos";
+import { useRenderLoop } from "@tresjs/core";
+import { shallowRef, watch } from "vue";
+
+const { scene: planet } = await useGLTF(
+	"https://raw.githubusercontent.com/Tresjs/assets/main/models/gltf/low-poly/planet.gltf"
+);
+
+const { pane } = useTweakPane();
+
+const planetRef = shallowRef();
+
+planet.traverse((child) => {
+	if (child.isMesh) {
+		child.receiveShadow = true;
+	}
+});
+
+watch(
+	() => planetRef.value,
+	(planet) => {
+		if (!planet) return;
+		pane.addInput(planetRef.value, "visible", { label: "Planet" });
+	}
+);
+
+const { onLoop } = useRenderLoop();
+
+onLoop(({ delta }) => {
+	if (!planet) return;
+	planet.rotation.y += delta * 0.04;
+	planet.rotation.z += delta * 0.02;
+	planet.rotation.x += delta * 0.05;
+	planet.updateMatrixWorld();
+});
 </script>
 <script lang="ts">
 export default {
@@ -12,10 +48,28 @@ export default {
 </script>
 <template>
 	<div class="three-d">
-		<TresCanvas window-size>
+		<!--
+			// let scene = new THREE.Scene();
+// let loader = new FBXLoader();
+// loader.load("/Charizard/CharizardMegaX.FBX", function (object) {
+// 	scene.add(object);
+// });
+
+		-->
+		<TresCanvas clear-color="#88F">
 			<TresPerspectiveCamera :position="[0, 0, 5]" />
-			<OrbitControls />
-			<Circle :args="[1, 32]" v-log:material :position="[-5, -5, 0]" />"
+			<OrbitControls :enable-zoom="false" />
+			<primitive :object="model" />
+			<TresMesh ref="planetRef" v-bind="planet" />
+			<TresMesh :rotation="[Math.PI / 3, 0, 0]" :position="[-2, -2, 0]">
+				<TresCircleGeometry :args="[0.7, 32]" />
+				<TresMeshBasicMaterial color="#0f0" />
+			</TresMesh>
+			<TresMesh :rotation="[-Math.PI / 2.2, 0, 0]" :position="[2, 0, 0]">
+				<TresCircleGeometry :args="[0.7, 32]" />
+				<TresMeshBasicMaterial color="#0f0" />
+			</TresMesh>
+			<!-- <TresMesh v-bind="scene" /> -->
 		</TresCanvas>
 	</div>
 </template>
@@ -24,5 +78,6 @@ export default {
 	border: 10px solid red;
 	width: fit-content;
 	height: 270px;
+	margin: 0px;
 }
 </style>
