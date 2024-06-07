79fc9911b2bfe3bf775173b8d5277a8c7f572f4f
BhJaipa <105307427+BhJaipal@users.noreply.github.com>
2024-06-07 13:26:30 +0530

    menu and navigation

--- src/components/threeD.vue
+++ src/components/threeD.vue
@@ -18,7 +18,7 @@ export default {
 };
 </script>
 <script setup lang="ts">
-import { onMounted, reactive, ref } from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
 import * as THREE from "three";
 import { fireTypes, grassTypes } from "./../data/pokemons";
 import { posAdd, resultAttackResultEffect } from "./../utils/pokemon-types";
@@ -33,42 +33,56 @@ let gl = {
 let focusedNo = ref(0);
 let focused = ref<null | HTMLButtonElement>(null);
 onMounted(() => {
+	// @ts-ignore
 	focused.value = document.getElementsByClassName("moves")[focusedNo.value];
 	focused.value?.focus();
 });
 document.onkeydown = (e: KeyboardEvent) => {
-	console.log(e);
 	if (e.key == "Enter" || e.key == "a") {
 		e.preventDefault();
-		let moveFocused = players.p1.moves.find(
-			(el) => focused.value?.textContent?.match(el.moveName)?.length != 0
-		);
-		if (moveFocused) {
-			fight(moveFocused);
-		}
+		focused.value?.click();
+		watch(usingMove, (val) => {
+			if (val == false) {
+				focused.value?.focus();
+			}
+		});
 	} else if (e.code == "ArrowDown") {
-		if (focusedNo.value == players.p1.moves.length - 1) {
+		if (movesShow.value && focusedNo.value == players.p1.moves.length - 1) {
+			focusedNo.value = 0;
+		} else if (focusedNo.value == 3) {
 			focusedNo.value = 0;
 		} else {
 			focusedNo.value++;
 		}
+		// @ts-ignore
 		focused.value =
 			document.getElementsByClassName("moves")[focusedNo.value];
 		focused.value?.focus();
 	} else if (e.code == "ArrowUp") {
-		if (focusedNo.value == 0) {
+		if (focusedNo.value == 0 && movesShow.value) {
 			focusedNo.value = players.p1.moves.length - 1;
+		} else if (focusedNo.value == 0) {
+			focusedNo.value = 3;
 		} else {
 			focusedNo.value--;
 		}
+		//@ts-ignore
 		focused.value =
 			document.getElementsByClassName("moves")[focusedNo.value];
 		focused.value?.focus();
+	} else if (e.key == "s") {
+		movesShow.value = false;
+		setTimeout(() => {
+			// @ts-ignore
+			focused.value =
+				document.getElementsByClassName("moves")[focusedNo.value];
+			focused.value?.focus();
+		}, 500);
 	}
 };
 let sleep = ref(1000);
 let players = reactive({
-	p1: fireTypes.charmander,
+	p1: fireTypes.charmeleon,
 	p1Hp: 200,
 	p2Hp: 200,
 	p2: grassTypes.venasaur,
@@ -194,6 +208,39 @@ function fight(move: PokemonMove) {
 	}, sleep.value);
 	sleep.value += 1000;
 }
+let movesShow = ref(false);
+let menuOptions = [
+	{
+		name: "Fight",
+		onclick: () => {
+			movesShow.value = true;
+			setTimeout(() => {
+				// @ts-ignore
+				focused.value =
+					document.getElementsByClassName("moves")[focusedNo.value];
+				focused.value?.focus();
+			}, 500);
+		},
+	},
+	{
+		name: "Bag",
+		onclick: () => {
+			console.log("Not available yet");
+		},
+	},
+	{
+		name: "Switch",
+		onclick: () => {
+			console.log("Not available yet");
+		},
+	},
+	{
+		name: "Run",
+		onclick: () => {
+			console.log("Not available yet");
+		},
+	},
+];
 </script>
 <template>
 	<div class="three-d">
@@ -201,40 +248,60 @@ function fight(move: PokemonMove) {
 			{{ resultShow.message }}
 		</div>
 		<div class="options">
-			<button
-				v-for="(move, i) in players.p1.moves"
-				:key="i"
-				:disabled="usingMove"
-				@click="fight(move)"
-				:class="'w-full moves ' + move.moveType"
-			>
-				<div>
-					<img
-						class="move-type"
-						:src="'/types-icon/' + move.moveType + '.svg'"
-						:alt="move.moveType"
-					/>
-				</div>
-				<div class="flex flex-col move-name-box">
-					<div class="move-name">{{ move.moveName }}</div>
+			<template v-if="movesShow">
+				<button
+					v-for="(move, i) in players.p1.moves"
+					:key="i"
+					:disabled="usingMove"
+					@click="fight(move)"
+					:class="'w-full moves ' + move.moveType"
+				>
 					<div>
-						{{
-							howMuchEffective(
-								attackResultEffect(move, players.p2)
-							)
-						}}
+						<img
+							class="move-type"
+							:src="'/types-icon/' + move.moveType + '.svg'"
+							:alt="move.moveType"
+						/>
 					</div>
-				</div>
-			</button>
+					<div class="flex flex-col move-name-box">
+						<div class="move-name">{{ move.moveName }}</div>
+						<div>
+							{{
+								howMuchEffective(
+									attackResultEffect(move, players.p2)
+								)
+							}}
+						</div>
+					</div>
+				</button>
+			</template>
+			<template v-else>
+				<button
+					class="w-full menu-opt moves"
+					v-for="(option, i) in menuOptions"
+					:key="i"
+					@click="option.onclick"
+				>
+					{{ option.name }}
+				</button>
+			</template>
 		</div>
 		<div class="p1-hp">
 			<div>HP</div>
-			<v-progress-linear v-model="HPs.p1" height="25" color="green">
+			<v-progress-linear
+				v-model="HPs.p1"
+				height="15"
+				:color="HPs.p1 > 45 ? 'green' : HPs.p1 < 15 ? 'red' : 'yellow'"
+			>
 			</v-progress-linear>
 		</div>
 		<div class="p2-hp">
 			<div>HP</div>
-			<v-progress-linear v-model="HPs.p2" height="25" color="green">
+			<v-progress-linear
+				v-model="HPs.p2"
+				height="15"
+				:color="HPs.p2 > 45 ? 'green' : HPs.p2 < 15 ? 'red' : 'yellow'"
+			>
 			</v-progress-linear>
 		</div>
 		<TresCanvas clear-color="#88F" v-bind="gl" window-size>
@@ -279,7 +346,7 @@ function fight(move: PokemonMove) {
 				:intensity="5"
 				cast-shadow
 			/>
-			<TresAmbientLight :intensity="1" />
+			<TresAmbientLight :intensity="2" :position="[-2, 2, 0]" />
 		</TresCanvas>
 	</div>
 </template>
@@ -332,9 +399,10 @@ function fight(move: PokemonMove) {
 	position: absolute;
 	z-index: 1;
 	display: flex;
-	width: 25vw;
+	width: 15vw;
 	flex-direction: row;
-	left: 20vw;
+	left: 25vw;
+	font-size: x-small;
 	margin-top: 30vh;
 }
 .p1-hp > div:first-child {
@@ -364,9 +432,15 @@ function fight(move: PokemonMove) {
 	z-index: 1;
 	flex-direction: row;
 	display: flex;
-	width: 25vw;
-	left: 55vw;
-	margin-top: 30vh;
+	width: 15vw;
+	left: 57vw;
+	font-size: x-small;
+	margin-top: 35vh;
+}
+.menu-opt {
+	background-color: burlywood !important;
+	font-size: larger;
+	font-weight: bolder;
 }
 .moves {
 	border-image: radial-gradient(to bottom right, #ff0000, #00ff00) 1;
